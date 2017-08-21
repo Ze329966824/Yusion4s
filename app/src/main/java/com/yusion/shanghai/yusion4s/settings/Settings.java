1ef869f6742caf359a252f1408de7a5d56d2475a
天青色 <ice45571@163.com>
2017-08-21 11:06:52 +0800

    2017年08月21日11:06:48 测试环境显示

--- app/src/main/java/com/yusion/shanghai/yusion4s/settings/Settings.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/settings/Settings.java
@@ -5,11 +5,12 @@ package com.yusion.shanghai.yusion4s.settings;
  */
 
 public class Settings {
+    public static boolean isOnline = false;
+    public static boolean isShameData = false;
 //    public static String SERVER_URL = "http://192.168.0.203:8000/";
-    public static String SERVER_URL = "http://api.yusiontech.com:8000/";
+public static String SERVER_URL = isOnline ? "http://api.yusiontech.com:8000/" : "http://api.alpha.yusiontech.com:8000/";
     //public static String SERVER_URL = "http://192.168.0.203:8000";
     public static String TEST_TOKEN = "94332209404d79860339284100000000";
     public static String OSS_SERVER_URL = "http://oss.yusiontech.com:9100";
-    public static boolean isOnline = true;
-    public static boolean isShameData = false;
+
 }
