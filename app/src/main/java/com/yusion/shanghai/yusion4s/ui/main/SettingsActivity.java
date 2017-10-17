44adfb8967dcb7ba37f20d199e8ee46a0323f54a
ze329966824 <ze329966824>
2017-10-17 10:11:35 +0800

    2017-10-17

--- app/src/main/java/com/yusion/shanghai/yusion4s/ui/main/SettingsActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/ui/main/SettingsActivity.java
@@ -93,7 +93,8 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
                     //product：调用oss接口更新
                     AuthApi.update(this, "yusion4s", data -> {
                         if (data != null) {
-                            if (!versionCode.contains(data.version)) {
+                            int result = splitVersion(versionCode.substring(1)).compareTo(splitVersion(data.version));
+                            if (result < 0) {
                                 UpdateUtil.showUpdateDialog(SettingsActivity.this, data.change_log, false, data.download_url);
                             } else {
                                 Toast.makeText(this, "已经是最新的版本啦！", Toast.LENGTH_SHORT).show();
@@ -170,4 +171,11 @@ public class SettingsActivity extends BaseActivity implements View.OnClickListen
             }
         });
     }
+    private String splitVersion(String s) {
+        String ss = null;
+        char[] str = s.toCharArray();
+        ss = String.valueOf(str[0]) + String.valueOf(str[2]) + String.valueOf(str[4]);
+        return ss;
+    }
+
 }
