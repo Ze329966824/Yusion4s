0bc6f4d51f49f2c2111db2f6e3bdb17585ae5aff
ze329966824 <ze329966824>
2017-10-26 16:33:31 +0800

    2017年10月26日

--- app/src/main/java/com/yusion/shanghai/yusion4s/ui/order/OrderDetailActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/ui/order/OrderDetailActivity.java
@@ -238,6 +238,24 @@ public class OrderDetailActivity extends BaseActivity {
 
             order_detail_sign_layout.setVisibility(View.GONE);
             order_detail_change_layout.setVisibility(View.VISIBLE);
+        orderDetailChangeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(OrderDetailActivity.this, .class);
+//                intent.putExtra("app_id", app_id);
+//                startActivity(intent);
+            }
+        });
+        orderDetailUploadBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                Intent intent = new Intent(OrderD
+//                        etailActivity.this, SubmitInformationActivity.class);
+//                intent.putExtra("app_id", app_id);
+//                startActivity(intent);
+            }
+        });
+
     }
 
     private void initData() {
