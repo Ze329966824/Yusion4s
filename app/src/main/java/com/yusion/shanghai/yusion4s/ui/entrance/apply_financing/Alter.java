e495ca55bb100b7667d695b6f970149234adf66a
aa <1043261302@qq.com>
2017-10-27 18:04:14 +0800

    2017年10月27日18:04:02

--- app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/apply_financing/Alter.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/apply_financing/Alter.java
@@ -1,15 +1,12 @@
 package com.yusion.shanghai.yusion4s.ui.entrance.apply_financing;
 
-import android.app.Dialog;
 import android.graphics.Color;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
-import android.text.style.ForegroundColorSpan;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
@@ -22,7 +19,6 @@ import android.widget.Toast;
 import com.yusion.shanghai.yusion4s.R;
 import com.yusion.shanghai.yusion4s.Yusion4sApp;
 import com.yusion.shanghai.yusion4s.base.BaseActivity;
-import com.yusion.shanghai.yusion4s.base.BaseResult;
 import com.yusion.shanghai.yusion4s.bean.dlr.GetBrandResp;
 import com.yusion.shanghai.yusion4s.bean.dlr.GetDlrListByTokenResp;
 import com.yusion.shanghai.yusion4s.bean.dlr.GetLoanBankResp;
@@ -30,7 +26,6 @@ import com.yusion.shanghai.yusion4s.bean.dlr.GetModelResp;
 import com.yusion.shanghai.yusion4s.bean.dlr.GetRawCarInfoResp;
 import com.yusion.shanghai.yusion4s.bean.dlr.GetTrixResp;
 import com.yusion.shanghai.yusion4s.bean.dlr.GetproductResp;
-import com.yusion.shanghai.yusion4s.retrofit.Api;
 import com.yusion.shanghai.yusion4s.retrofit.api.DlrApi;
 import com.yusion.shanghai.yusion4s.retrofit.api.OrderApi;
 import com.yusion.shanghai.yusion4s.retrofit.callback.OnCodeAndMsgCallBack;
@@ -40,10 +35,6 @@ import com.yusion.shanghai.yusion4s.utils.wheel.WheelViewUtil;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class Alter extends BaseActivity {
     public static int DELAY_MILLIS;
@@ -1016,7 +1007,7 @@ public class Alter extends BaseActivity {
                 return true;
             }
         }
-        Toast.makeText(Alter.this,"S",Toast.LENGTH_LONG).show();
+        Toast.makeText(Alter.this, "S", Toast.LENGTH_LONG).show();
         return false;
     }
 
