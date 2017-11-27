af3d3bb09cc5ab82bfdc830e3418c9d7750a4d31
ze329966824 <ze329966824>
2017-11-27 15:05:57 +0800

    车辆估值ui

---
+++ app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/AppraisalvalueActivity.java
@@ -0,0 +1,15 @@
+package com.yusion.shanghai.yusion4s.ui.entrance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yusion.shanghai.yusion4s.R;
+
+public class AppraisalvalueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_appraisalvalue);
+    }
+}
