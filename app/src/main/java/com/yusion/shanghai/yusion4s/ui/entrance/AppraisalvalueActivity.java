378c5506664645ce9051b179b5700be81faa3dc6
shauna <shauna>
2017-11-27 18:58:25 +0800

    估值界面

--- app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/AppraisalvalueActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/AppraisalvalueActivity.java
@@ -2,13 +2,18 @@ package com.yusion.shanghai.yusion4s.ui.entrance;
 
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.shizhefei.view.largeimage.LargeImageView;
 import com.yusion.shanghai.yusion4s.R;
 import com.yusion.shanghai.yusion4s.utils.Base64Util;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AppraisalvalueActivity extends AppCompatActivity {
     private LargeImageView appraisal_value_img;
@@ -42,11 +47,15 @@ public class AppraisalvalueActivity extends AppCompatActivity {
 
 
 
-        appraisal_download_tv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Base64Util.saveBaseImage(baseStr,getPackageResourcePath(),"appraisalvalue.png");
-            }
+        appraisal_download_tv.setOnClickListener(v -> {
+            Date date=new Date();
+            DateFormat format=new SimpleDateFormat("yyyy-MM-dd");
+            String time=format.format(date);
+            String name = time +".png";
+
+            Base64Util.saveBaseImage(baseStr,Environment.getExternalStorageDirectory().getPath()+"/yusion/",name);
+            Log.e("TAG", "path : "+Environment.getExternalStorageDirectory().getPath());
+
         });
 
 
