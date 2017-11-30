47e5ef0b05a0b3e4ab3bf5e21ad15a5bc3d1eaa3
shauna <shauna>
2017-11-30 17:18:16 +0800

    base64

--- app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/AppraisalvalueActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/AppraisalvalueActivity.java
@@ -12,7 +12,9 @@ import com.shizhefei.view.largeimage.LargeImageView;
 import com.yusion.shanghai.yusion4s.R;
 import com.yusion.shanghai.yusion4s.base.BaseActivity;
 import com.yusion.shanghai.yusion4s.bean.upload.ListImgsReq;
+import com.yusion.shanghai.yusion4s.bean.upload.ListImgsResp;
 import com.yusion.shanghai.yusion4s.retrofit.api.UploadApi;
+import com.yusion.shanghai.yusion4s.retrofit.callback.OnItemDataCallBack;
 import com.yusion.shanghai.yusion4s.utils.Base64Util;
 
 import java.io.IOException;
@@ -30,6 +32,9 @@ public class AppraisalvalueActivity extends BaseActivity {
 
 
     private String baseStr;
+    private Bitmap bitmap;
+    private URL finalUrl;
+    private URL url;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,7 +57,17 @@ public class AppraisalvalueActivity extends BaseActivity {
 //                appraisal_value_img.setImage(bitmap);
 //            }
 //        });
+        appraisal_download_tv.setOnClickListener(v -> {
+            Date date = new Date();
+            DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+            String time = format.format(date);
+            String name = time + ".png";
 
+            Base64Util.saveBaseImage(baseStr, Environment.getExternalStorageDirectory().getPath() + "/yusion/", name);
+            Toast.makeText(this,"截图已保存到"+Environment.getExternalStorageDirectory().getPath()+ "/yusion/"+name,Toast.LENGTH_SHORT).show();
+            Log.e("TAG", "path : " + Environment.getExternalStorageDirectory().getPath());
+
+        });
 
         baseStr = getIntent().getStringExtra("guess_img");
         if (baseStr != null) {
@@ -67,40 +82,42 @@ public class AppraisalvalueActivity extends BaseActivity {
             req.label = getIntent().getStringExtra("label");
             Log.e("TAG", req.clt_id + req.app_id + req.role + req.label);
 
-            UploadApi.listImgs(this, req, data -> {
-                if (data.list.isEmpty() || data.list.size() < 1) {
-                    return;
-                }
-                URL url = null;
-                try {
-                    url = new URL(data.list.get(0).s_url);
-                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                    conn.setConnectTimeout(5 * 1000);
-                    InputStream inputStream = conn.getInputStream();
-                    Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                    appraisal_value_img.setImage(bitmap);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                } catch (IOException e) {
-                    e.printStackTrace();
+
+            UploadApi.listImgs(this, req, new OnItemDataCallBack<ListImgsResp>() {
+                @Override
+                public void onItemDataCallBack(ListImgsResp data) {
+                    if (data.list.isEmpty() || data.list.size() < 0) {
+                        Toast.makeText(AppraisalvalueActivity.this,"并没有图片",Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    try {
+                        url = new URL(data.list.get(0).s_url);
+                        finalUrl = url;
+                        new Thread(() -> setUrl(new OnItemDataCallBack() {
+                            @Override
+                            public void onItemDataCallBack(Object data) {
+                                appraisal_value_img.setImage(bitmap);
+                            }
+                        }));
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
                 }
             });
         }
-
-
-        appraisal_download_tv.setOnClickListener(v -> {
-            Date date = new Date();
-            DateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-            String time = format.format(date);
-            String name = time + ".png";
-
-            Base64Util.saveBaseImage(baseStr, Environment.getExternalStorageDirectory().getPath() + "/yusion/", name);
-            Toast.makeText(this,"截图已保存到"+Environment.getExternalStorageDirectory().getPath()+ "/yusion/"+name,Toast.LENGTH_SHORT).show();
-            Log.e("TAG", "path : " + Environment.getExternalStorageDirectory().getPath());
-
-        });
-
-
+    }
+    private void setUrl(OnItemDataCallBack callBack) {
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) finalUrl.openConnection();
+            conn.setConnectTimeout(5 * 1000);
+            InputStream inputStream = null;
+            inputStream = conn.getInputStream();
+            bitmap = BitmapFactory.decodeStream(inputStream);
+            callBack.onItemDataCallBack(true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
