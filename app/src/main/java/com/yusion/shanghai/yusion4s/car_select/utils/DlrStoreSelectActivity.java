1def4131d10c0cfdd56f870b2e8664dc61558faf
aa <1043261302@qq.com>
2017-12-11 15:12:30 +0800

    选择门店

---
+++ app/src/main/java/com/yusion/shanghai/yusion4s/car_select/utils/DlrStoreSelectActivity.java
@@ -0,0 +1,15 @@
+package com.yusion.shanghai.yusion4s.car_select.utils;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yusion.shanghai.yusion4s.R;
+
+public class DlrStoreSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dlr_store_select);
+    }
+}
