bcef4bd9b30650827a0591116637997c831e8ad7
shauna <shauna>
2017-12-13 10:27:30 +0800

    门店 -> 经销商

--- app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/apply_financing/CarInfoFragment.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/ui/entrance/apply_financing/CarInfoFragment.java
@@ -330,6 +330,7 @@ public class CarInfoFragment extends BaseFragment {
     }
 
 
+    
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
