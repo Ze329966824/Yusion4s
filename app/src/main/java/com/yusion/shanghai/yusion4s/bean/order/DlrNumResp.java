649f572787d3a5c97bdb083713bb72aa9375119d
shauna <shauna>
2018-01-03 19:11:55 +0800

    首页四张卡片及跳转

--- app/src/main/java/com/yusion/shanghai/yusion4s/bean/order/DlrNumResp.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/bean/order/DlrNumResp.java
@@ -8,21 +8,21 @@ import com.google.gson.Gson;
 
 public class DlrNumResp {
 
-    /**
-     * reject_count : 0
-     * dealing_count : 5
-     * to_be_confirm_count : 2
-     * to_be_upload_count : 0
-     * to_loan_count : 1
-     * today_count : 0
-     * all_count : 6
-     */
+  /**
+   * reject_count : 0
+   * dealing_count : 5
+   * to_be_confirm_count : 2
+   * to_be_upload_count : 0
+   * to_loan_count : 1
+   * today_count : 0
+   * all_count : 6
+   */
 
-  public String reject_count;
   public String dealing_count;
-  public String to_be_confirm_count;
-  public String to_be_upload_count;
-  public String to_loan_count;
+  public String to_be_confirm_count;   //放款审核
+  public String to_loan_count;         //放款
+  public String to_be_upload_count;    //贷后追踪
+  public String reject_count;           //已拒绝
   public String today_count;
   public String all_count;
 
