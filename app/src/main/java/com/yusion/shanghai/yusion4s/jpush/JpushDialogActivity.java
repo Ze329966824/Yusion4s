1fd8c69520a809dc5f4d42a195fea573769a36a2
shauna <shauna>
2018-01-15 18:22:39 +0800

    集成新推送样式

--- app/src/main/java/com/yusion/shanghai/yusion4s/jpush/JpushDialogActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/jpush/JpushDialogActivity.java
@@ -1,13 +1,23 @@
 package com.yusion.shanghai.yusion4s.jpush;
 
+import android.animation.ObjectAnimator;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.yusion.shanghai.yusion4s.R;
 import com.yusion.shanghai.yusion4s.Yusion4sApp;
@@ -19,9 +29,7 @@ import com.yusion.shanghai.yusion4s.utils.PopupDialogUtil;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static com.yusion.shanghai.yusion4s.R.id.btn_cancel;
-
-public class JpushDialogActivity extends BaseActivity {
+public class JpushDialogActivity extends BaseActivity implements View.OnTouchListener {
     private String username = null;
     private String mobile = null;
     private String title = null;
@@ -31,6 +39,10 @@ public class JpushDialogActivity extends BaseActivity {
     private String category = null;
     private String stringExtra = null;
     private String order_state = null;
+    private View contentView;
+    private View rootview;
+    private PopupWindow mPopWindow;
+    private SoundPool soundPool;
 
 /*    "reg_id":xxxx,
             "mobile": 138xxx,
@@ -45,14 +57,25 @@ public class JpushDialogActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_jpush_dialog);
         try {
+
+            init();
             initJpush();
         } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
+    private void init() {
+        contentView = LayoutInflater.from(JpushDialogActivity.this).inflate(R.layout.popuplayout, null);
+        soundPool = new SoundPool(10, AudioManager.STREAM_SYSTEM, 5);
+        //加载deep 音频文件
+        soundPool.load(this, R.raw.push_sound, 1);
+        contentView.setOnTouchListener(this);
+    }
+
 
     private void initJpush() throws JSONException {
+
         Intent intent = getIntent();
         if (intent != null) {
             stringExtra = intent.getStringExtra("jsonObject");
@@ -77,7 +100,7 @@ public class JpushDialogActivity extends BaseActivity {
         if (Yusion4sApp.isLogin && username.equals(Yusion4sApp.ACCOUNT)) {
             switch (category) {
                 case "login"://抢登
-                    PopupDialogUtil.showOneButtonDialog(this, content,"好的", dialog -> {
+                    PopupDialogUtil.showOneButtonDialog(this, content, "好的", dialog -> {
                         myApp.clearUserData();
                         startActivity(new Intent(JpushDialogActivity.this, LoginActivity.class));
                         finish();
@@ -86,8 +109,8 @@ public class JpushDialogActivity extends BaseActivity {
                 case "application":
                     switch (order_state) {
                         case "pass":
-                            JpushApproveDialog approvePassDialog = new JpushApproveDialog(this,title,content,R.layout.dialog_approval_pass);
-                            approvePassDialog.setYesOnclickListener(() ->{
+                            JpushApproveDialog approvePassDialog = new JpushApproveDialog(this, title, content, R.layout.dialog_approval_pass);
+                            approvePassDialog.setYesOnclickListener(() -> {
                                 approvePassDialog.dismiss();
                                 Intent intent = new Intent(JpushDialogActivity.this, OrderDetailActivity.class);
                                 intent.putExtra("app_id", app_id);
@@ -101,8 +124,8 @@ public class JpushDialogActivity extends BaseActivity {
                             approvePassDialog.show();
                             break;
                         case "refuse":
-                            JpushApproveDialog approveRefuseDialog = new JpushApproveDialog(this,title,content,R.layout.dialog_approval_refuse);
-                            approveRefuseDialog.setYesOnclickListener(() ->{
+                            JpushApproveDialog approveRefuseDialog = new JpushApproveDialog(this, title, content, R.layout.dialog_approval_refuse);
+                            approveRefuseDialog.setYesOnclickListener(() -> {
                                 approveRefuseDialog.dismiss();
                                 Intent intent = new Intent(JpushDialogActivity.this, OrderDetailActivity.class);
                                 intent.putExtra("app_id", app_id);
@@ -129,6 +152,9 @@ public class JpushDialogActivity extends BaseActivity {
                     }
 
                     break;
+                case "test":
+
+                    showPopupWindow();
 
                 default:
                     new AlertDialog.Builder(JpushDialogActivity.this)
@@ -147,6 +173,106 @@ public class JpushDialogActivity extends BaseActivity {
         }
     }
 
+    private void showPopupWindow() {
+
+        contentView.setVisibility(View.VISIBLE);
+        mPopWindow = new PopupWindow(contentView,
+                ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT, true);
+        mPopWindow.setContentView(contentView);
+        mPopWindow.setAnimationStyle(R.style.mypopwindow_anim_style);
+        mPopWindow.setFocusable(false);
+        mPopWindow.setOutsideTouchable(false);
+
+        //显示PopupWindow
+        rootview = LayoutInflater.from(JpushDialogActivity.this).inflate(R.layout.activity_main, null);
+        mPopWindow.showAtLocation(rootview, Gravity.TOP, 0, 0);
+
+
+//播放deep
+        soundPool.play(1, 1, 1, 0, 0, 1);
+        contentView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(JpushDialogActivity.this, "点击了一下", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        int lastX = 0, lastY = 0;
+
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        int screenWidth = dm.widthPixels;
+        int screenHeight = dm.heightPixels;
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                lastX = (int) event.getRawX();//获取触摸事件触摸位置的原始X坐标
+                lastY = (int) event.getRawY();
+
+            case MotionEvent.ACTION_MOVE:
+                //event.getRawX();获得移动的位置
+                int dx = (int) event.getRawX() - lastX;
+                int dy = (int) event.getRawY() - lastY;
+                float scaleY = v.getScaleY();
+                int height = v.getHeight();
+                int width = v.getWidth();
+
+                if (Math.abs(dy) > Math.abs(dx)) {
+
+                    mPopWindow.dismiss();
+                    v.scrollTo(0, 0);
+                    v.setAlpha(1);
+
+                } else {
+
+                    v.scrollBy(-dx, 0);
+
+                    float abs_x = Math.abs(v.getScrollX());
+//                                Log.e("TAG", "onTouch: " + abs_x); //0 -> 600+
+                    v.setAlpha(1.0f - 1.0f / width * abs_x);
+
+                }
+
+
+                lastX = (int) event.getRawX();
+                lastY = (int) event.getRawY();
+                break;
+            case MotionEvent.ACTION_UP:
+                int i = screenWidth / 2;
+                ObjectAnimator animator = ObjectAnimator.ofFloat(v, "alpha", v.getAlpha(), 1.0f);
+                final int scrollX = v.getScrollX();
+                animator.addUpdateListener(animation -> {
+                    float fraction = animation.getAnimatedFraction();
+                    v.setScrollX((int) (scrollX * (1.0f - fraction)));
+                    Log.e("TAG", "onAnimationUpdate: " + fraction);
+                });
+                animator.start();
+
+                if (Math.abs(v.getScrollX()) > i) {
+                    v.setAlpha(0);
+                    v.postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            mPopWindow.dismiss();
+                            v.scrollTo(0, 0);
+                            v.setAlpha(1);
+                        }
+                    }, 1);
+                } else {
+                    v.scrollTo(0, 0);
+                    v.setAlpha(1);
+                }
+                break;
+
+            default:
+                break;
+        }
+
+        return true;
+    }
 
 
     // pass 和 refuse  一个dialog  传入不同的布局即可
@@ -240,8 +366,6 @@ public class JpushDialogActivity extends BaseActivity {
             resID = resid;
         }
     }
-
-
 
 
 }
