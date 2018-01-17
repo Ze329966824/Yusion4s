ebdadde65d1004f8655b04fb5ddce8bb28cb47d8
iceIC <ice45571@163.com>
2018-01-17 12:09:34 +0800

    沉浸式

--- app/src/main/java/com/yusion/shanghai/yusion4s/base/BaseActivity.java
+++ app/src/main/java/com/yusion/shanghai/yusion4s/base/BaseActivity.java
@@ -17,6 +17,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.PopupWindow;
@@ -57,19 +58,23 @@ public class BaseActivity extends AppCompatActivity implements View.OnClickListe
 //        UBT.bind(this);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
-            getWindow().setStatusBarColor(Color.TRANSPARENT);
-        }
-        else {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
+        } else {
             getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
         }
 
     }
 
+
     @Override
     protected void onStart() {
         super.onStart();
-//        ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0).setFitsSystemWindows(true);
+        ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0).setFitsSystemWindows(true);
     }
 
     public TitleBar initTitleBar(final Activity activity, String title) {
@@ -267,12 +272,21 @@ public class BaseActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void showPopupWindow() {
-
+        int oldSystemUiVisibility = getWindow().getDecorView().getSystemUiVisibility();
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
         contentView.setVisibility(View.VISIBLE);
         mPopWindow = new PopupWindow(contentView, ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.WRAP_CONTENT, true);
         mPopWindow.setContentView(contentView);
         mPopWindow.setAnimationStyle(R.style.mypopwindow_anim_style);
         mPopWindow.setFocusable(false);
+        mPopWindow.setClippingEnabled(false);
+        mPopWindow.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                getWindow().getDecorView().setSystemUiVisibility(oldSystemUiVisibility);
+
+            }
+        });
         mPopWindow.setOutsideTouchable(false);
 
 
@@ -281,7 +295,7 @@ public class BaseActivity extends AppCompatActivity implements View.OnClickListe
 
         contentView.postDelayed(() -> {
 //            mPopWindow.showAtLocation(getWindow().getDecorView(), Gravity.TOP, 0, (int) getResources().getDimension(R.dimen.y50));
-            mPopWindow.showAtLocation(getWindow().getDecorView(), Gravity.TOP, 0,0);
+            mPopWindow.showAtLocation(getWindow().getDecorView(), Gravity.TOP, 0, 0);
 
         }, 1000);
 
